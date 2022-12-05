6cbd2a55c85613b66774e8cbda2f9f772741f75d
Karatsuban <75843468+Karatsuban@users.noreply.github.com>
2022-12-05 21:22:17 +0100

    Day5
    Pretty fun day !

---
+++ 2022/day5/day5-1.py
@@ -0,0 +1,26 @@
+import re
+
+L_stacks = [[] for _ in range(9)]
+
+with open("input5.txt") as file:
+    content = file.readlines()
+    for line in content[:8]:
+        line = line.replace("\n", "")
+        disp = re.sub("    ", " [#]", line)
+        disp = re.sub("(\[|\])", "", disp)
+        vals = disp.split()
+        
+        for idx, a in enumerate(vals):
+            if a != "#":
+                L_stacks[idx].insert(0,a)
+        
+
+    for line in content[10:]:
+        cmd = line.split()
+        nb, orig, dest = int(cmd[1]), int(cmd[3])-1, int(cmd[5])-1
+        for a in range(nb):
+            val = L_stacks[orig].pop(-1)
+            L_stacks[dest].append(val)
+
+for a in range(len(L_stacks)):
+    print(L_stacks[a][-1], end="")
